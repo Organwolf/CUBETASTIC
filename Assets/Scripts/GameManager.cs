ffaed070593c2e426e6de60a9dfc779c36394c07
Aron <aron.polner@gmail.com>
2019-06-26 09:59:43 +0200

    Working project w multiple scenes and animated GUI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1;
+    public GameObject completeLevelUI;
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            // Restart the game
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+        Debug.Log("Completed Level!");
+    }
+}
